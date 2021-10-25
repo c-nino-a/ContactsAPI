d5fb8f3217c2505335680f3580a76d54a040ac58
Christian Nino G. Arrieta <57577523+c-nino-a@users.noreply.github.com>
2021-10-25 11:15:47 +0800

    Added e-mail address features

--- public/person-form.js
+++ public/person-form.js
@@ -1,5 +1,46 @@
 $(function() {
 
+    var eatbl = $('.person-form .ea-tbl')
+
+    function removeEaTr(btn) {
+        btn = $(btn)
+
+        var tr = btn.closest('tr')
+
+        tr.remove()
+    }
+
+    $('.person-form .ea-btn-add').on('click',
+        function() {
+            // var btn = $(this)
+
+            var html =
+                `<tr>
+            <td>
+              <a href="javascript:" class='ui button icon ea-btn-remove'>
+                   <i class='icon close'></i>
+                </a>
+             </td>
+             <td class='field'>
+                <input type='text' name='emailaddresses' value>
+    
+            </td>
+        </tr>`
+            html = $(html)
+            eatbl.append(html)
+            html.find('.ea-btn-remove').on('click', function() {
+
+                removeEaTr(this)
+
+            })
+        })
+
+    $('.person-form .ea-btn-remove').on('click',
+        function() {
+            removeEaTr(this)
+
+        })
+
 
     $('.person-form').on('submit', function(e) {
 
